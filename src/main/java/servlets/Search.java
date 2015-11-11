5d1490b583d0453390b83dde30b0983606c2db62
Manyanda Chitimbo <chitson1990@gmail.com>
2015-11-12 00:08:03 +0100

    find method fixes. Search servlets added..

---
+++ src/main/java/servlets/Search.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Machi on 12/11/2015.
+ */
+public class Search extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
